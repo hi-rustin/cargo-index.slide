0093d8154fc262ea9f65000cd75b31578b423634
hi-rustin <rustin.liu@gmail.com>
2023-09-10 15:17:07 +0800

    cargo-index: add links
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- components/Timeline.vue
+++ components/Timeline.vue
@@ -14,31 +14,31 @@
     <a-timeline>
       <a-timeline-item>
         2019-10-18
-        <text color="gray"><a>@kornelski</a> Submitted RFC</text>
+        <text color="gray"><a href="https://www.github.com/kornelski">@kornelski</a> Submitted RFC</text>
       </a-timeline-item>
       <a-timeline-item>
         2020-11-24
-        <text color="gray"><a>@jonhoo</a> Submitted the first implementation <a>Cargo/PR#8890</a></text>
+        <text color="gray"><a href="https://www.github.com/jonhoo">@jonhoo</a> Submitted the first implementation <a href="https://www.github.com/rust-lang/Cargo/pull/8890">Cargo/PR#8890</a></text>
       </a-timeline-item>
       <a-timeline-item>
         2021-01-03
-        <text color="gray"><a>@ehuss</a> Accepted the RFC</text>
+        <text color="gray"><a href="https://www.github.com/ehuss">@ehuss</a> Accepted the RFC</text>
       </a-timeline-item>
       <a-timeline-item>
         2022-03-10
-        <text color="gray"><a>@arlosi</a> Submitted the final implementation <a>Cargo/PR#10470</a> based on <a>Cargo/PR#8890</a></text>
+        <text color="gray"><a href="https://www.github.com/arlosi">@arlosi</a> Submitted the final implementation <a  href="https://www.github.com/rust-lang/Cargo/pull/10470">Cargo/PR#10470</a> based on <a  href="https://www.github.com/rust-lang/Cargo/pull/8890">Cargo/PR#8890</a></text>
       </a-timeline-item>
       <a-timeline-item>
         2022-03-23
-        <text color="gray"><a>@arlosi</a> Submitted the implementation in crates.io side <a>crates.io/PR#4661</a> </text>
+        <text color="gray"><a href="https://www.github.com/arlosi">@arlosi</a> Submitted the implementation in crates.io side <a  href="https://www.github.com/rust-lang/crates.io/pull/4661">crates.io/PR#4661</a> </text>
       </a-timeline-item>
       <a-timeline-item>
         2022-06-22
-        <text color="gray"><a>@arlosi</a> Published <a>Call for testing: Cargo sparse-registry</a> blog post</text>
+        <text color="gray"><a href="https://www.github.com/arlosi">@arlosi</a> Published <a href="https://blog.rust-lang.org/2022/06/22/sparse-registry-testing.html">Call for testing: Cargo sparse-registry</a> blog post</text>
       </a-timeline-item>
       <a-timeline-item>
         2023-01-20
-        <text color="gray"><a>@arlosi</a> Stabilized Sparse Registry Feature in <a>Cargo/PR#11224</a></text>
+        <text color="gray"><a href="https://www.github.com/arlosi">@arlosi</a> Stabilized Sparse Registry Feature in <a href="https://www.github.com/rust-lang/Cargo/pull/11224">Cargo/PR#11224</a></text>
       </a-timeline-item>
     </a-timeline>
 </template>
